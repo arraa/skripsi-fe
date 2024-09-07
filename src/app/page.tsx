e150dc7b09086dd6cb231ede3c668904a99d8650
arraa <tiararahmah82@gmail.com>
2024-09-08 00:49:00 +0700

    remove get data in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
-import { getStudent } from "./api/student";
+
 
 export default async function Home() {
-
-  const response = await getStudent();
-      console.log(response.data);
   return (
       <>
           holaaaa
