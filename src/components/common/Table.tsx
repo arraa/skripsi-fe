a07b8aca1b8ecdbc5daeceb50831c26776d40a93
Rifian Fernando <rifianfernando19@gmail.com>
2024-11-17 11:44:38 +0700

    style(layout): improve code formatting and indentation in RootLayout component

--- src/components/common/Table.tsx
+++ src/components/common/Table.tsx
@@ -1,36 +1,36 @@
 import { DataGrid, GridColDef } from '@mui/x-data-grid';
 
 interface tableProps {
-  data: Array<object>;
-  columnData: GridColDef[];
-  searchValue?: string;
+    data: Array<object>;
+    columnData: GridColDef[];
+    searchValue?: string;
 }
 const Table = (props: tableProps) => {
-  const { data, columnData, searchValue } = props;
-  return (
-    <DataGrid
-      rows={data}
-      columns={columnData}
-      initialState={{
-        pagination: { paginationModel: { pageSize: 25 } },
-      }}
-      pageSizeOptions={[25, 50, 75]}
-      disableRowSelectionOnClick
-      filterModel={{
-        items: [],
-        quickFilterValues: [searchValue],
-      }}
-       sx={{
-        maxWidth:'80vw',
-        border: 'none',
-        boxShadow: 'none',
-        color: '#0c427770',
-        '& .MuiDataGrid-columnHeaderTitle': {
-          fontWeight: 'bold',
-        },
-      }}
-    />
-  );
+    const { data, columnData, searchValue } = props;
+    return (
+        <DataGrid
+            rows={data}
+            columns={columnData}
+            initialState={{
+                pagination: { paginationModel: { pageSize: 25 } },
+            }}
+            pageSizeOptions={[25, 50, 75]}
+            disableRowSelectionOnClick
+            filterModel={{
+                items: [],
+                quickFilterValues: [searchValue],
+            }}
+            sx={{
+                maxWidth: '80vw',
+                border: 'none',
+                boxShadow: 'none',
+                color: '#0c427770',
+                '& .MuiDataGrid-columnHeaderTitle': {
+                    fontWeight: 'bold',
+                },
+            }}
+        />
+    );
 };
 
 export default Table;
