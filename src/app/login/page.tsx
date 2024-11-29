fac897bb0ac41f7c5361df272496b4e34530efb8
arraa <tiararahmah82@gmail.com>
2024-11-29 23:13:04 +0700

    feat: add login and forget password page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,8 @@
+import ForgetPassword from '@/components/auth/ForgetPassword';
+import CheckEmail from '@/components/auth/CheckEmail';
+import Login from '@/components/auth/Login';
+import ResetPassword from '@/components/auth/ResetPassword';
+
+export default function personalData() {
+    return <ResetPassword />;
+}
