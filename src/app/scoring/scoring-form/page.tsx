f30d4e0a031ae3b0d81c9fed22a502e1b68ea877
Rifian Fernando <rifianfernando19@gmail.com>
2024-12-08 23:17:14 +0700

    feat: add Suspense for lazy loading in scoring form component

--- src/app/scoring/scoring-form/page.tsx
+++ src/app/scoring/scoring-form/page.tsx
@@ -1,9 +1,12 @@
 import { AuthProvider } from '@/app/context/provider';
 import ScoringForm from '@/components/scoring/ScoringForm';
+import { Suspense } from 'react';
 export default function scoringForm() {
     return (
         <AuthProvider>
-            <ScoringForm/>
+            <Suspense fallback={<div>Loading...</div>}>
+                <ScoringForm />
+            </Suspense>
         </AuthProvider>
-    );
+    )
 }
