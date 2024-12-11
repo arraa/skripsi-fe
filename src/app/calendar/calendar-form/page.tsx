118781387fd6b1456a7054bbc6e7af7d4790074c
arraa <tiararahmah82@gmail.com>
2024-12-12 01:05:22 +0700

    fix : import calender form

--- src/app/calendar/calendar-form/page.tsx
+++ src/app/calendar/calendar-form/page.tsx
@@ -1,11 +1,11 @@
-import calenderForm from '@/components/calender/calenderForm'
+import CalendarForm from '@/components/calender/CalenderForm'
 import { Suspense } from 'react'
 
 export default function calender() {
     return (
         <>
             <Suspense fallback={<div>Loading...</div>}>
-                <calenderForm />
+                <CalendarForm/>
             </Suspense>
         </>
     )
